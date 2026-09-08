c9b18cd0511f4bfb73252212b166a7bed7d97b21
Mani246-sido <maninder13905@gmail.com>
2026-09-08 14:56:19 +0530

    Update GitHub Actions workflow for portfolio deployment and configure Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
+    base: "/mani-portfolio/",
     plugins: [
         glsl({
             include: ['**/*.glsl', '**/*.vert', '**/*.frag'],
